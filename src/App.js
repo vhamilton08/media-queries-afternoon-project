99d62a430794cc2426b157cf9a48068d44e1b4e9
vhamilton08 <vhamilton08@gmail.com>
2020-08-04 18:45:12 -0500

    afternoon work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { 
+      dropDownVisible: false
+
+    }
+  }
+  toggleDropDown = () => {
+    this.setState({dropDownVisible: !this.state.dropDownVisible})
+  }
+
+    render () {
+      return (
+
+        <div>
+          <div className="main-image">
+
+          <header className="header">
+            <img alt="logo" className="logo" src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>
+            <nav className="desktop-menu">
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+            <div className="dropdown" onClick={this.toggleDropDown}>Menu</div>
+            {this.state.dropDownVisible ? (
+            <nav className="mobile-menu">
+            <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+            ) : null }
+           </header>
+           <div className="main-text">
+             <h2>Welcome To Our Studio!</h2>
+             <h1>IT'S NICE TO MEET YOU</h1>
+             <button>TELL ME MORE</button>
+           </div>
+          </div>
+           <div className="content2">
+             <h1>Services</h1>
+             <p>Lorem ipsum dolor sit amet consectetur.</p>
+           </div>
+        </div>
+
+      )
+  }
 }
+
+
+
 
 export default App;
